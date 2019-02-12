907481ad7863770cf1e34220c55210a680aafca0
Kemele Endris <endris@l3s.de>
2019-02-12 20:58:39 +0100

    Setup.py for Ontario installation

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='ontario',
+      version='1.0',
+      description='Ontario - Ontology-based architecture for semantic data lakes ',
+      author='Kemele M. Endris',
+      author_email='endris@L3s.de',
+      url='https://github.com/SDM-TIB/Ontario',
+      scripts=['runExperiment.py', 'run_query.py', 'runOntarioExp.sh'],
+      packages=find_packages(exclude=['docs']),
+      install_requires=["ply",
+                        'hdfs',
+                        'pyspark',
+                        'pymongo',
+                        'mysql-connector-python',
+                        'neo4j-driver',
+                        'rdflib',
+                        'networkx',
+                        'enum'],
+      include_package_data=True,
+      license='GNU/GPL v2'
+     )
