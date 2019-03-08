ff8e3bbbb0c56775cc7f56ef7ba50d6d676f633b
keme686 <Keme702755>
2019-03-08 14:07:45 +0100

    upgrade ubuntu and python

--- run_query.py
+++ run_query.py
@@ -1,127 +1,46 @@
-from ontario.config import OntarioConfiguration
-from ontario.mediator.Decomposer import LakeCatalyst
-from ontario.mediator.Decomposer import MetaCatalyst
-from ontario.mediator.Planner import LakePlanner
-
-from ontario.config.model import DataSourceType
-import ontario.sparql.utilities as utils
-import getopt, sys
-from ontario.mediator import Catalyst
 
 
-def get_options(argv):
-    try:
-        opts, args = getopt.getopt(argv, "h:q:c:p:s:")
-    except getopt.GetoptError:
-        usage()
-        sys.exit(1)
+if __name__ == '__main__':
+    from pprint import pprint
+    # query = open("../queries/simpleQueries/SQ9").read()
+    query = open("../queries/complexqueries/CQ9").read()
 
-    query = None
-    '''
-    Supported output formats:
-        - json (default)
-        - nt
-        - SPARQL-UPDATE (directly store to sparql endpoint)
-    '''
-    config = 'config/config.json'
-    isstring = False
-    planonly = False
-    for opt, arg in opts:
-        if opt == "-h":
-            usage()
-            sys.exit()
-        elif opt == "-q":
-            query = arg
-        elif opt == "-c":
-            config = arg
-        elif opt == '-p':
-            planonly = eval(arg)
-        elif opt == '-s':
-            isstring = eval(arg)
-
-    if not query:
-        usage()
-        sys.exit(1)
-
-    if not isstring:
-        query = open(query).read()
-
-    return query, config, planonly
-
-
-def usage():
-    usage_str = ("Usage: {program} -q <query>  \n"
-                 "-c <path/to/config.json> \n"
-                 "-p <planOnly?> \n"
-                 "where \n"
-                 "\t<query> - SPARQL QUERY  \n"                 
-                 "\t<path/to/config.json> - path to configuration file  \n"
-                 "\t<planOnly?> - True to show only execution plan, False Otherwise \n")
-
-    print(usage_str.format(program=sys.argv[0]),)
-
-
-def finalize(processqueue):
-    import os
-    p = processqueue.get()
-
-    while p != "EOF":
-        try:
-            os.kill(p, 9)
-        except OSError as ex:
-            pass
-        p = processqueue.get()
-
-
-if __name__ == "__main__":
-
-    from time import time
-    # query, config, planonly = get_options(sys.argv[1:])
-    query = open('/home/kemele/git/MULDER/queries/polyweb/QE-3').read()
-    # query = 'select distinct * where{?cnv <http://sels.insight.org/cancer-genomics/schema/disease> ?disease .	' \
-    #         'FILTER (?disease = "Lung cancer" )} limit 10'
-    config = 'scripts/polyweb-polystore.json'
-    planonly = False
-    ct = time()
-    configuration = OntarioConfiguration(config)
-    start = time()
-    print("reading config finished! It took: (sec) ", (start-ct))     #, configuration.datasources)
-    dc = Catalyst(query, configuration)
-    # pprint.pprint(configuration.metadata)
-    decomp = LakeCatalyst(dc.query, dc.config)
-    dc.query = decomp.query
-    decomposed_query = decomp.decompose()
-    print("---------Decomposed query -------")
-    import pprint
-    pprint.pprint(decomposed_query)
-    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-
-    quers = dc.catalyze(decomposed_query)
-
-    pprint.pprint(quers)
-    dectime = time() - start
-    print("Decomposition time: ", dectime)
-    if len(quers) == 0:
-        print('No results')
-        exit()
-    planstart = time()
-    pl = LakePlanner(dc.query, quers, configuration)
-    tree = pl.make_tree()
-
-    print("0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0")
-    print(pl.query)
-    print("777777777777777777---PLAN----777777777777777777777777777")
-
-    plan = pl.make_plan()
-    pprint.pprint(plan)
-    plantime = time() - planstart
-    print("Decomposition time: ", dectime)
-    print("Planning time: ", plantime)
-    if planonly:
-        exit()
-
+    from ontario.config import OntarioConfiguration
+    from ontario.mediator.Planner import MetaWrapperPlanner
+    from ontario.mediator.Decomposer import *
     from multiprocessing import Queue
+    from time import time
+    from pprint import pprint
 
+    # configfile = '../configurations/lslod-rdf-distributed.json'
+    configfile = '../configurations/lslod_rdf_rdb.json'
+    # configfile = '../configurations/lslod_rdf_rdb_tsv_json.json'
+    configuration = OntarioConfiguration(configfile)
+    start = time()
+    print("\nDecomposition:\n")
+
+    mc = MediatorCatalyst(query, configuration)
+    r = mc.decompose()
+    query = mc.query
+    # r = decomposeUnionBlock(query.body, configuration, prefixes)
+    planonly = False
+    pprint(r)
+    print("Decomposition : -----------------")
+    print(query)
+    dectime = time() - start
+    planstart = time()
+    print("Plan : -----------------")
+
+    mwp = MetaWrapperPlanner(query, r, configuration)
+    # query.body = UnionBlock(mwp.make_tree())
+    # query.body = UnionBlock(create_plan_tree(r, configuration))
+
+    # pl = LakePlanner(query, r, configuration)
+
+    plan = mwp.make_plan()
+    print(plan)
+    plantime = time() - planstart
+    #exit()
     out = Queue()
     processqueue = Queue()
     plan.execute(out, processqueue)
@@ -130,7 +49,7 @@ if __name__ == "__main__":
     i = 0
 
     while r != 'EOF':
-        pprint.pprint(r)
+        # pprint(r)
         r = out.get()
         i += 1
 
@@ -140,4 +59,4 @@ if __name__ == "__main__":
     print("Decomposition time: ", dectime)
     print("Planning time: ", plantime)
     print("total exe time:", exetime)
-    finalize(processqueue)
+
