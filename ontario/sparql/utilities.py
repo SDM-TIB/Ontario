6fa9d070b20e5a4972b5caad5eb64865dc053cdb
Kemele M. Endris <keme686@gmail.com>
2019-05-09 14:52:17 +0200

    cleanup files

--- ontario/sparql/utilities.py
+++ ontario/sparql/utilities.py
@@ -51,3 +51,65 @@ def getTemplatesPerMolecule(molecules):
             else:
                 moltemp[m] = [t.pred]
     return moltemp
+
+
+def aux(e ,x, op):
+
+    def pp(t):
+        return t.show(x + "  ")
+    if type(e) == tuple:
+        (f, s) = e
+        r = ""
+        if f:
+            r = x + "{\n" + aux(f, x + "  ", op) + "\n" + x + "}\n"
+        if f and s:
+            r = r + x + op + "\n"
+        if s:
+            r = r + x + "{\n" + aux(s,x + "  ", op) + "\n" + x +"}"
+        return r
+    elif type(e) == list:
+        return (x + " . \n").join(map(pp, e))
+    elif e:
+        return e.show(x + "  ")
+
+    return ""
+
+
+def aux2(e,x, op):
+    def pp (t):
+        return t.show2(x + "  ")
+    if type(e) == tuple:
+        (f, s) = e
+        r = ""
+        if f:
+            r = x + "{\n" + aux2(f, x + "  ", op) + "\n" + x + "}\n"
+        if f and s:
+            r = r + x + op + "\n"
+        if s:
+            r = r + x + "{\n" + aux2(s, x + "  ", op) + "\n" + x + "}"
+        return r
+    elif type(e) == list:
+        return (x + " . \n").join(map(pp, e))
+    elif e:
+        return e.show2(x + "  ")
+    return ""
+
+
+def nest(l):
+
+    l0 = list(l)
+    while len(l0) > 1:
+        l1 = []
+        while len(l0) > 1:
+            x = l0.pop()
+            y = l0.pop()
+            l1.append((x,y))
+        if len(l0) == 1:
+            l1.append(l0.pop())
+        l0 = l1
+    if len(l0) == 1:
+        return l0[0]
+    else:
+        return None
+
+
