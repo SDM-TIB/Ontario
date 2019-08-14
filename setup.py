9b74e95e5443a97a2f9fb453f6d076b1a57f56a7
Kemele M. Endris <keme686@gmail.com>
2019-08-14 17:07:22 +0200

    varsion and requirements added

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='ontario',
-      version='1.0',
+      version='0.3',
       description='Ontario - Ontology-based architecture for semantic data lakes',
       author='Kemele M. Endris',
-      author_email='endris@l3s.de',
+      author_email='kemele.endris@gmail.com',
       url='https://github.com/SDM-TIB/Ontario',
       scripts=['scripts/runExperiment.py', 'run_query.py', 'scripts/runOntarioExp.sh', 'scripts/run_dief_experiment.py'],
       packages=find_packages(exclude=['docs']),
       install_requires=["ply",
+                        "flask",
+                        "requests",
                         'hdfs',
                         'pyspark',
                         'pymongo',
